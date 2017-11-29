8d6b5792d5a3c04bedbe09324d4c998300dcbbd0
Brian Dear <superacidjax@me.com>
2017-11-29 17:55:36 +0000

    adds motd and changes default apache for nodes

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -5,8 +5,15 @@
 # Copyright:: 2017, Brian Dear, All Rights Reserved.
 #
 
+case node['platform_family']
+when 'rhel'
+  package = 'httpd'
+when 'debian'
+  package = 'apache2'
+end
+
 package 'httpd' do
-  package_name 'httpd'
+  package_name package
   action :install
 end
 
@@ -14,3 +21,4 @@ service 'apache2' do
   service_name 'httpd'
   action [:start, :enable]
 end
+
